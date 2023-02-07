66d3745acad76816b89414202841d6205e385b06
zalobalay8 <92026671+zalobalay8@users.noreply.github.com>
2023-02-07 20:14:15 +0100

    player animations

--- Assets/Scripts/JohnMovement.cs
+++ Assets/Scripts/JohnMovement.cs
@@ -7,24 +7,37 @@ public class JohnMovement : MonoBehaviour
     public float speed;
     public float jump_force;
     private Rigidbody2D rigidbody2D;
+    private Animator animator;
     private float horizontal;
     private bool suelo;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D=GetComponent<Rigidbody2D>();
+        animator=GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         horizontal=Input.GetAxisRaw("Horizontal");
+
+        if (horizontal<0.0f){
+            transform.localScale=new Vector3(-1.0f, 1.0f, 1.0f);
+        }else if (horizontal>0.0f){
+            transform.localScale=new Vector3(1.0f, 1.0f, 1.0f);
+        }
+
+
+        animator.SetBool("running", horizontal != 0.0f);
+
         if(Physics2D.Raycast(transform.position, Vector3.down, 0.1f)){
             suelo=true;
         }else{
             suelo=false;
         }
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) && suelo)
         {
             Jump();
         }
@@ -35,8 +48,8 @@ public class JohnMovement : MonoBehaviour
         rigidbody2D.AddForce(Vector2.up*jump_force);
     }
 
-    void fixedUpdate(){
-        rigidbody2D.velocity=new Vector2(horizontal, rigidbody2D.velocity.y);
+    void FixedUpdate(){
+        rigidbody2D.velocity=new Vector2(horizontal*speed, rigidbody2D.velocity.y);
 
     }
 }
