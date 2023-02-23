eefaf0a9072801c8989b44680bd5fd975865695c
zalobalay8 <92026671+zalobalay8@users.noreply.github.com>
2023-02-23 09:21:24 +0100

    main scene

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -7,7 +7,12 @@ public class CameraScript : MonoBehaviour
     public GameObject john;
     // Update is called once per frame
     void Update()
-    {
+    {   
+        if(Pause_script.isGamePaused){
+            GetComponent<AudioSource>().Pause();
+        }else if(!Pause_script.isGamePaused && !GetComponent<AudioSource>().isPlaying){ 
+            GetComponent<AudioSource>().Play();
+        }
         if(john==null){return;}
         Vector3 position = transform.position;
         position.x = john.transform.position.x;
